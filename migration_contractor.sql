f7abe3daf278192881fce9e6628e2756d5e7e947
Lukasz Brzyski <lukaszbrzyski@MacBook-Air-Lukasz.local>
2026-06-13 09:41:33 +0200

    merge sqli

--- migration_contractor.sql
+++
@@ -1,7 +0,0 @@
--- Migracja: dodanie pól kontrahenta i numeru faktury do tabeli expenses
--- Uruchomić przez phpMyAdmin na bazie brzychu_roslinarnia
-
-ALTER TABLE expenses
-    ADD COLUMN IF NOT EXISTS contractor_name    VARCHAR(256) AFTER description,
-    ADD COLUMN IF NOT EXISTS contractor_nip     VARCHAR(32)  AFTER contractor_name,
-    ADD COLUMN IF NOT EXISTS invoice_number     VARCHAR(128) AFTER contractor_nip;
